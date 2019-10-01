550b541dca91919f2678f156deb3641106b3c577
artqz <djoctuk@yandex.ru>
2019-10-01 12:29:01 +0500

    test walk

--- src/App.js
+++ src/App.js
@@ -4,23 +4,43 @@ import { Map } from 'immutable';
 function App() {
   const [count, setCount] = useState(() =>
     Map({
-      playerX: 20,
-      playerY: 10
+      playerDirection: 1,
+      playerX: 2,
+      playerY: 40,
+      playerWidth: 16,
+      playerHeight: 16,
+      messgae: ''
     })
   );
   const requestRef = React.useRef();
   const previousTimeRef = React.useRef();
 
+  // Слушаем и записываем нажатые клавиши
+  let buttons = new Set();
+  window.addEventListener('keydown', e => buttons.add(e.keyCode));
+  window.addEventListener('keyup', e => buttons.delete(e.keyCode));
+
   const update = time => {
     if (previousTimeRef.current != undefined) {
       const deltaTime = time - previousTimeRef.current;
+      let move = 0;
+      let top = 4;
+      let tick = 1;
+      let message = '';
 
-      // Pass on a function to the setter of the state
-      // to make sure we always have the latest state
+      if (buttons.has(37)) {
+        move = -1;
+        console.log('лево');
+      } else if (buttons.has(39)) {
+        move = 1;
+      } else if (buttons.has(32)) {
+        console.log('прыжок');
+      }
+
       setCount(pCount =>
-        pCount
-          .update('playerX', value => value + deltaTime * 0.15)
-          .update('playerY', value => value + deltaTime * 0.04)
+        pCount.update('playerX', value =>
+          move ? value + deltaTime * 0.15 * move : value
+        )
       );
     }
     previousTimeRef.current = time;
